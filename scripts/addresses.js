3c94ad5a1e7320ff58942fbbb25bc30c414d7156
Dameon Mendoza <dameonmendoza@gmail.com>
2022-04-12 08:14:52 -0700

    added client side

---
+++ scripts/addresses.js
@@ -0,0 +1,6 @@
+let address = {
+    something: "something",
+    deploying_contracts_with_account: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+    account_balance: "10000000000000000000000",
+    coffee_portal_address: "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+}
--- scripts/run.js
+++ scripts/run.js
@@ -1,12 +1,47 @@
 const main = async () => {
-
-    // This will compile our contract and generate the necessary files we need to work with our contract under the artifacts directory.
-    const coffeeContractFactory = await hre.ethers.getContractFactory('CoffeePortal');
-    const coffeeContract = await coffeeContractFactory.deploy();
-
-    await coffeeContract.deployed(); // We'll wait until the network deploys our contract to our local blockchain! Our constructor runs when we deploy.
-
+    const coffeeContractFactory = await hre.ethers.getContractFactory(
+        "CoffeePortal"
+    );
+    const coffeeContract = await coffeeContractFactory.deploy({
+        value: hre.ethers.utils.parseEther("0.1"),
+    });
+    await coffeeContract.deployed();
     console.log("Coffee Contract deployed to:", coffeeContract.address);
+
+    /*
+     * Get Contract balance
+     */
+    let contractBalance = await hre.ethers.provider.getBalance(
+        coffeeContract.address
+    );
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
+
+    /*
+     * Let's try to buy a coffee
+     */
+    const coffeeTxn = await coffeeContract.buyCoffee(
+        "This is coffee #1",
+        "idris",
+        ethers.utils.parseEther("0.001")
+    );
+    await coffeeTxn.wait();
+
+    /*
+     * Get Contract balance to see what happened!
+     */
+    contractBalance = await hre.ethers.provider.getBalance(
+        coffeeContract.address
+    );
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
+
+    let allCoffee = await coffeeContract.getAllCoffee();
+    console.log(allCoffee);
 };
 
 const runMain = async () => {
